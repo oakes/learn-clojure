82b79f14eaf509ca2c5ca6100eac2a6e63eeac51
oakes <zach@sekao.net>
2019-12-13 15:00:15 -0500

    Use multimethod for routing

--- src/example/core_server.clj
+++ src/example/core_server.clj
@@ -1,9 +1,49 @@
 (ns example.core-server
-  (:require [org.httpkit.server :as server]))
+  (:require [org.httpkit.server :as server]
+            [hiccup.core :as hiccup]
+            [clojure.string :as str]))
 
-(defn handler [request]
-  (println "Received request:")
-  (println request))
+(defn get-people []
+  (let [people (slurp "docs/people.csv")
+        people (str/split-lines people)
+        header (first people)
+        people (rest people)
+        header (str/split header #",")
+        header (map keyword header)
+        people (map
+                 (fn [person]
+                   (zipmap header (str/split person #",")))
+                 people)]
+    people))
+
+(defmulti handler :uri)
+
+(defmethod handler "/" [request]
+  {:status 200
+   :body (hiccup/html
+           [:html
+            [:body
+             [:ol
+              (map
+                (fn [person]
+                  [:li (:first_name person)])
+                (get-people))]]])})
+
+(defmethod handler "/philippines" [request]
+  {:status 200
+   :body (hiccup/html
+           [:html
+            [:body
+             [:ol
+              (->> (get-people)
+                   (filter
+                     (fn [person]
+                       (= (:country person)
+                          "Philippines")))
+                   (map
+                     (fn [person]
+                       [:li (:first_name person)])))]]])})
+
 
 (defn -main []
   (server/run-server (var handler) {:port 3000})
